f9c65452d3b4840742a5a440ea9c7749c5718e00
HeQing <tsinghe.7@gmail.com>
2015-05-27 17:42:00 +0800

    :)

---
+++ pytest/fib.py
@@ -0,0 +1,11 @@
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+
+
+if __name__ == '__main__':
+    for n in fib(10):
+        print n, ""
