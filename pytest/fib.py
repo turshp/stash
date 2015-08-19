1c8ea9111bc99bd2a7ecae4c23a307333a9894c4
HeQing <tsinghe.7@gmail.com>
2015-08-19 13:51:41 +0800

    ....

--- pytest/fib.py
+++ pytest/fib.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+
+import pdb
+
 def fib(max):
     n, a, b = 0, 0, 1
     while n < max:
         yield b
+        pdb.set_trace()
         a, b = b, a + b
         n = n + 1
 
